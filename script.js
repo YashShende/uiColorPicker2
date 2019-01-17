4598eb29e551939426c48e27327ea7e63e49b3a3
Yash.js <46022781+YashShende@users.noreply.github.com>
2019-01-17 23:56:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//need to be done
+$(document).ready(function () {
+
+      $(".card").click(function () {
+
+
+                 var hex = this.textContent.trim();
+                 var dummy = document.createElement("input");
+
+                 document.body.appendChild(dummy);
+                 dummy.setAttribute('value', hex);
+                 dummy.select();
+                 document.execCommand("copy");
+                 document.body.removeChild(dummy);
+
+                 swal("Copied!", {
+                     icon: "success",
+                     buttons: false,
+                     timer: 1000,
+                 });
+      });
+});
